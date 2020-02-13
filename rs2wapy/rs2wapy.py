4a271ebbaf8d0e159b293af95814fd7642d85f3f
Tuomo <tuokri@tuta.io>
2020-02-13 15:10:47 +0200

    Interface definitions

--- rs2wapy/rs2wapy.py
+++ rs2wapy/rs2wapy.py
@@ -27,5 +27,14 @@ class RS2WebAdmin(object):
     def current_game(self) -> models.CurrentGame:
         return self._adapter.get_current_game()
 
-    def players(self) -> models.Players:
+    def current_players(self) -> models.Players:
         return self.current_game().get_players()
+
+    def banned_players(self) -> models.Players:
+        return self.current_game().get_banned_players()
+
+    def tracked_players(self) -> models.Players:
+        return self.current_game().get_tracked_players()
+
+    def access_policy(self) -> models.AccessPolicy:
+        return self._adapter.get_access_policy()
