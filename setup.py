d7e67a87c6efd52d4e5883eb5bef6c8a0332903d
Tuomo <tuokri@tuta.io>
2020-03-02 22:03:16 +0200

    Update requirements

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.6',
+    python_requires='>=3.7',
     install_requires=[
         "beautifulsoup4",
         "bs4",
@@ -23,5 +23,6 @@ setuptools.setup(
         "pycurl",
         "soupsieve",
         "requests",
+        "steam",
     ]
 )
