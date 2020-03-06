a27ca14c51eb526657d02416916be910e6477030
Tuomo <tuokri@tuta.io>
2020-03-06 17:03:46 +0200

    Fix grammar in setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import versioneer
 
 long_description = """
 Provides programmable interface to Rising Storm 2: Vietnam
-WebAdmin for server management automation building applications.
+WebAdmin for building server management automation applications.
 
 The library uses PycURL internally to communicate with RS2
 WebAdmin.
