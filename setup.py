44efc8590fc2db946c90fc2cd2a8de1f0ec060af
Tuomo <tuokri@tuta.io>
2020-03-06 17:01:38 +0200

    Update setup.py long_description

--- setup.py
+++ setup.py
@@ -2,6 +2,14 @@ import setuptools
 
 import versioneer
 
+long_description = """
+Provides programmable interface to Rising Storm 2: Vietnam
+WebAdmin for server management automation building applications.
+
+The library uses PycURL internally to communicate with RS2
+WebAdmin.
+"""
+
 setuptools.setup(
     name="rs2wapy",
     version=versioneer.get_version(),
@@ -10,6 +18,7 @@ setuptools.setup(
     author="tuokri",
     author_email="tuokri@tuta.io",
     description="Rising Storm 2: Vietnam WebAdmin Python Interface",
+    long_description=long_description,
     keywords="automation webadmin ue3",
     classifiers=[
         "Programming Language :: Python :: 3.7",
@@ -17,7 +26,7 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.7',
+    python_requires=">=3.7",
     install_requires=[
         "beautifulsoup4",
         "bs4",
