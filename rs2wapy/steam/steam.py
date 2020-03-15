974b62e91399e0f895a32e9c298fa0b9ddc8657b
Tuomo <tuokri@tuta.io>
2020-03-15 02:28:27 +0200

    Handle no API key

--- rs2wapy/steam/steam.py
+++ rs2wapy/steam/steam.py
@@ -21,29 +21,40 @@ class Singleton(type):
             if cls not in cls._instances:
                 cls._instances[cls] = super().__call__(
                     steam_api_key, *args, **kwargs)
-            return cls._instances[cls]
         except KeyError as ke:
             logger.info("'STEAM_WEB_API_KEY' environment variable not set, "
                         "some features are not available")
             logger.debug(ke)
+            cls._instances[cls] = super().__call__(*args, dummy=True, **kwargs)
         except requests.exceptions.HTTPError as e:
             logger.debug(e, exc_info=True)
             logger.warning("unable to initialize Steam Web API, "
                            "some features are not available")
+            cls._instances[cls] = super().__call__(*args, dummy=True, **kwargs)
+        return cls._instances[cls]
 
 
 class SteamWebAPI(steam.webapi.WebAPI, metaclass=Singleton):
     """Helper class for using Steam Web API quickly."""
+
     # TODO: Cache results.
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+    def __init__(self, *args, dummy=False, **kwargs):
+        self._dummy = dummy
+        if not dummy:
+            super().__init__(*args, **kwargs)
 
     def get_persona_name(self, steam_id: steam.SteamID) -> str:
-        return self.ISteamUser.GetPlayerSummaries(
-            steamids=steam_id.as_64)["response"]["players"][0]["personaname"]
+        if self._dummy:
+            return ""
+        else:
+            return self.ISteamUser.GetPlayerSummaries(
+                steamids=steam_id.as_64)["response"]["players"][0]["personaname"]
 
     def get_persona_names(self, steam_ids: Sequence[steam.SteamID]
                           ) -> Dict[steam.SteamID, str]:
+        if self._dummy:
+            return {steam_id: "" for steam_id in steam_ids}
+
         # TODO: handle more than 100 IDs.
         if len(steam_ids) > 100:
             raise ValueError("TODO: May request only 100 players at a time!")
